e7899390eeaa7ba406a7107267aac0c69e9bc70a
benjamin <benjamin.harlacher.external@atos.net>
2021-01-06 14:28:32 +0100

    clean rest api

---
+++ rest/src/main/java/com/example/rest/HeroDB.java
@@ -0,0 +1,20 @@
+package com.example.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class HeroDB {
+    @GetMapping("/heroname")
+    public Hero[] getHeroes()
+    {
+        return new Hero[]{  new Hero(1, "Dr Nice"),
+                new Hero(2,"Narco"),
+                new Hero(3,"Bombasto"),
+                new Hero(4,"Celeritas"),
+                new Hero(5,"Magneta"),
+                new Hero(6,"Magneta"),
+                new Hero(7,"Dynama"),
+                new Hero(8,"Dr IQ"),
+                new Hero(9,"Magma"),
+                new Hero(10,"Tornado")};
+    }
+}
