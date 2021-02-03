70d973110a97f8f6da2a7339388f40074da65499
BenjaminHarlacher <benjamin.harlacher.external@atos.net>
2021-02-03 09:46:49 +0100

    update

--- jwt/api/users/user.service.js
+++ jwt/api/users/user.service.js
@@ -18,5 +18,47 @@ module.exports = {
                 return callBack(null, results);
             }
         );
+    },
+    getusers: callBack =>{
+        pool.query(
+            `select id,firstName,lastName,email from tbl_user`,
+            [],
+            (error, results, fields) =>{
+                if (error){
+                   return callBack(error);
+                }
+                return callBack(null, results);
+            }
+        );
+    },
+    getuserByUserId:(id, callBack) =>{
+        pool.query(
+            `select id,firstName,lastName,email from tbl_user where id = ?`,
+            [id],
+            (error, results, fields) =>{
+                if (error){
+                   return callBack(error);
+                }
+                return callBack(null, results[0]);
+            }
+        );
+    },
+    updateUser:(data, callBack) =>{
+        pool.query(
+            `update registration set firstName=?, lastName=?, email=?,password=? where id = ?`,
+            [
+                data.first_name,
+                data.last_name,
+                data.email,
+                data.password,
+                data.id
+            ],
+            (error, results, fields) =>{
+                if (error){
+                   return callBack(error);
+                }
+                return callBack(null, results[0]);
+            }
+        );
     }
 };
